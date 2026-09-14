85ae9a3cc88422c3da0c64e67bab1bf88734f4aa
bruisedSoup <gonzales.isabella1414@gmail.com>
2026-09-14 15:09:13 +0800

    feat(components): export StatusModal in shared components index

--- src/components/index.js
+++ src/components/index.js
@@ -29,3 +29,5 @@ export { default as TableSearchBar } from "./TableSearchBar";
 export { default as Footer } from "./Footer";
 export { default as Header } from "./Header";
 export { default as Sidebar } from "./Sidebar";
+export { default as StatusModal } from "./StatusModal";
+
